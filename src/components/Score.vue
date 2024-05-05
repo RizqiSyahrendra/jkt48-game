e9424b872dd2f14f2ba44fef7f0ebb9db1d6118d
RizqiSyahrendra <rizqialaudinsyahrendra@gmail.com>
2024-05-05 08:03:56 +0700

    feat: Score component

---
+++ src/components/Score.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import images from '@/utils/images'
+const props = defineProps<{
+  value: number
+}>()
+
+const { value } = props
+
+function getScoreImages(val: number) {
+  if (val >= 90) {
+    return images.scoreTop
+  } else if (val >= 60) {
+    return images.scoreMiddle
+  } else {
+    return images.scoreBottom
+  }
+}
+</script>
+
+<template>
+  <div class="w-full flex flex-col justify-center items-center">
+    <img :src="getScoreImages(value)" class="w-48 h-44 object-cover rounded-[10px]" />
+    <span class="mt-2 font-bold text-primary4 text-2xl">Score : {{ value }}</span>
+  </div>
+</template>
